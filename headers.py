0d7c675a36ba028f7fc95e434f82432ce35ad0f5
Sarbaz_Vatan <33395636+SarbazVatan@users.noreply.github.com>
2018-07-06 03:40:14 -0700

    Create headers.py

---
+++ headers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+data = str(raw_input("\n[#]Header>>>\n\n")).decode('base64')
+
+data2 = data.split("\n")
+result = {}
+for x in data2:
+	value, key = x.strip("\r").replace(" ", '').split(":", 1)
+	result[value] = key
+with open('headers.txt', 'w') as hd:
+	hd.write(str(result))
+hd.close()
+os.popen("start headers.txt")
